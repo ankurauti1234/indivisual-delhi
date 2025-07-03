42de41a879f682eae973d3864b7468682ebe0258
Ankur Auti <ankur.auti@inditronics.com>
2025-07-03 10:58:52 +0530

    disclamer removed

--- src/app/(main)/dashboard/radio/broadcasters/weekly-analysis-fever/page.jsx
+++ src/app/(main)/dashboard/radio/broadcasters/weekly-analysis-fever/page.jsx
@@ -6,12 +6,12 @@ import RadioSectorAnalysis from './comperative-bar'
 import TopAdvertisersComparison from './top-advertisers-comparison'
 import TVChannelTreemap from './sectors-treemap'
 import DailyAdsLineChart from './daily-ads-line-chart'
-import { Table, AlertTriangle } from 'lucide-react'
+// import { Table, AlertTriangle } from 'lucide-react'
 
 const RadioDashboard = () => {
   return (
     <div className='space-y-6'>
-<div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 rounded-md flex items-start gap-3">
+{/* <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 rounded-md flex items-start gap-3">
   <AlertTriangle className="w-5 h-5 text-yellow-600 mt-0.5" />
   <div>
     <p className="text-sm font-medium text-yellow-800">
@@ -22,7 +22,7 @@ const RadioDashboard = () => {
       data for Radio One on 11th May and Radio Mirchi on 14th May at 10:00 hour was not avaliable.
     </p>
   </div>
-</div>
+</div> */}
       <StatCards />
       <RadioAdHeatmap />
       <RadioSectorAnalysis />
