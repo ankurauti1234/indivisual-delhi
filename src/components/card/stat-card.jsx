04b00214d9c003e7059da5fd54d1fc85e3c81f3d
Anand1305 <97216778+Anand1305@users.noreply.github.com>
2025-07-25 14:34:57 +0530

    descending order sort

--- src/components/card/stat-card.jsx
+++ src/components/card/stat-card.jsx
@@ -10,21 +10,22 @@ import { Separator } from "../ui/separator";
 
 
 const StatCard = ({ title, desc, keys, values }) => {
+  const sortedData = keys
+    .map((key, index) => ({ key, value: values[index] }))
+    .sort((a, b) => b.value - a.value); // sort descending by value
   return (
-    <Card className=" p-0 gap-0 w-full">
+    <Card className="p-0 gap-0 w-full">
       <CardHeader className="p-3">
         <CardTitle>{title}</CardTitle>
-        <CardDescription className=" truncate ">{desc}</CardDescription>{" "}
+        <CardDescription className="truncate">{desc}</CardDescription>
       </CardHeader>
       <Separator />
       <CardContent className="p-3 space-y-2">
         <ol className="space-y-1">
-          {keys.map((key, index) => (
+          {sortedData.map(({ key, value }, index) => (
             <li key={index} className="flex justify-between">
               <span className="text-base">{key}</span>
-              <span className="text-primary font-semibold ml-2">
-                {values[index]}
-              </span>
+              <span className="text-primary font-semibold ml-2">{value}</span>
             </li>
           ))}
         </ol>
